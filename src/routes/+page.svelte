61f492058fe246c480672f739974c4fb995e0312
Toni <antpiasecki@gmail.com>
2025-08-05 15:53:48 +0200

    star picker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+    import StarPicker from "$lib/StarPicker.svelte";
+
     const { data } = $props();
 </script>
+
+<svelte:head>
+    <title>Rate Stuff</title>
+</svelte:head>
 
 <div class="max-w-md mx-auto my-8">
     <a
         href="/add"
-        class="block text-center px-3 py-1 bg-gray-800 text-white text-sm rounded hover:bg-gray-700 cursor-pointer"
+        class="block text-center px-3 py-2 bg-gray-800 text-white text-sm rounded hover:bg-gray-700 cursor-pointer"
     >
         Add
     </a>
@@ -13,30 +19,25 @@
 
 {#each data.movies as movie}
     <div class="max-w-md mx-auto p-4 border rounded mb-3 flex gap-4">
-        <div class="w-16 h-16 flex-shrink-0">
+        <div class="w-16 h-auto flex-shrink-0">
             <img
                 src={movie.imdb_image}
                 alt=""
-                class="w-full h-full object-cover rounded-xl"
+                class="w-full object-cover rounded-xl"
             />
         </div>
         <div class="space-y-1">
             <div class="font-medium text-base">{movie.title}</div>
             <div class="text-gray-600 text-sm">{movie.year}</div>
-            <div class="text-gray-600 text-sm flex items-center gap-1">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    class="w-5 h-5 text-yellow-500"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                        clip-rule="evenodd"
-                    />
-                </svg>
-                <span class="font-medium">{movie.imdb_rating}</span>
+            <div class="flex gap-4">
+                <div class="text-gray-600 text-sm flex items-center gap-1">
+                    <img class="w-4 h-4" src="/imdb_logo.webp" alt="" />
+                    <span class="font-medium">{movie.imdb_rating}</span>
+                </div>
+                <div class="text-gray-600 text-sm flex items-center gap-1">
+                    <img class="w-4 h-4" src="/star.webp" alt="" />
+                    <span class="font-medium">0</span>
+                </div>
             </div>
         </div>
     </div>
